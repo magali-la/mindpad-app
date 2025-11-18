2d7c3175ba7131a969ebcf1747d294886e2e0673
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-18 10:21:08 -0500

    (local storage): create save post function to set items in local storage, window event listener to retrieve saved data on load

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let createPostForm = document.getElementById("createForm");
 let createTitleInput = document.getElementById("createTitle");
 let createContentInput = document.getElementById("createContent");
 
+// local storage
+let padPosts = [];
+
+window.addEventListener("load", () => {
+    padPosts = JSON.parse(localStorage.getItem('posts', JSON.stringify(padPosts)));
+    console.log("PadPosts retrieved from local storage", padPosts);
+})
+
 // form validity
 createPostForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -22,8 +30,27 @@ createPostForm.addEventListener("submit", (event) => {
         createContentInput.classList.remove('is-valid');
         // delay the reset to view the check
         setTimeout(() => {
-            createPostForm.classList.remove('was-validated');
-            createPostForm.reset();            
+            createPostForm.classList.remove('was-validated'); 
+            savePost();
+            // reset form and inputs
+            createPostForm.reset();  
         }, 800);
     }
 }, false);
+
+// function to save posts and update local storage
+function savePost() {
+    // set an object with the content
+    let newPost = {
+        id: Date.now(),
+        title: createTitleInput.value,
+        content: createContentInput.value
+    };
+    console.log(newPost);
+
+    // push it to the padPosts arr & store in local storage
+    padPosts.push(newPost);
+    console.log(padPosts);
+
+    localStorage.setItem('posts', JSON.stringify(padPosts));
+}
