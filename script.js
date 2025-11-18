6cf84a852846b107493a968bc548d58e1321dc55
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-17 22:15:12 -0500

    (validation): update valid styling, added delay for user success feedback and form reset

--- script.js
+++ script.js
@@ -5,9 +5,25 @@ let createContentInput = document.getElementById("createContent");
 
 // form validity
 createPostForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
     if (!createPostForm.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
-    } 
-    createPostForm.classList.add('was-validated');
+        // display error message if invalid
+        createPostForm.classList.add('was-validated');
+        console.log("Invalid fields");
+    } else { 
+        // display valid feedback
+        createTitleInput.classList.add('is-valid');
+        createContentInput.classList.add('is-valid');
+        createPostForm.classList.add('was-validated');
+        console.log("All inputs valid");
+        // set validation check on inputs 
+        createTitleInput.classList.remove('is-valid');
+        createContentInput.classList.remove('is-valid');
+        // delay the reset to view the check
+        setTimeout(() => {
+            createPostForm.classList.remove('was-validated');
+            createPostForm.reset();            
+        }, 800);
+    }
 }, false);
