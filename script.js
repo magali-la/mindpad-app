fe9ed426d7fb668f757698643a068c76af772758
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-21 23:46:31 -0500

    fix(save post): refactor to remove duplicate code and call displayPost function

--- script.js
+++ script.js
@@ -99,42 +99,12 @@ function savePost() {
 
     localStorage.setItem('posts', JSON.stringify(padPosts));
 
-    // now clone the template and add it to the grid by appending the list
-    let template = document.getElementById("templatePost");
-    let clonedPost = template.cloneNode(true);
-
-    // remove d-none class from clone to show it on the page
-    clonedPost.classList.remove('d-none');
-
-    // set a unique id for the post
-    clonedPost.setAttribute('data-post-id', newPost.id);
-
-    // define variables for the classes on the template
-    let padPostDate = clonedPost.querySelector('.padPostDate');
-    let padPostTitle = clonedPost.querySelector('.padPostTitle');
-    let padPostContent = clonedPost.querySelector('.padPostContent');
-
-    // change the date object to something more readable
-    let postDate = new Date(newPost.id);
-    // create a weekday Month and day
-    let weekday = postDate.toLocaleDateString('en-US', {weekday: 'short'});
-    let month = postDate.toLocaleDateString('en-US', {month: 'short', day: 'numeric'})
-    let time = postDate.toLocaleTimeString('en-US', {hour:'numeric', minute: '2-digit', hour12: true});
-    // format the date
-    let formattedDate = `${weekday} ${month} @ ${time}`;
-    padPostDate.innerText = formattedDate;
-
-    // set the value of the title input and content input as the saved post info
-    padPostTitle.value = newPost.title;
-    padPostContent.value = newPost.content;
-
-    // prepend the grid with the clonedtemplate to display it
-    padPostGrid.prepend(clonedPost);
+    // call displayPost to clone the template and display it
+    displayPost(newPost);
 }
 
 // function to display posts saved in local storage
 function displayPost(postObj) {
-
     // now clone the template and add it to the grid by appending the list
     let template = document.getElementById("templatePost");
     let clonedPost = template.cloneNode(true);
